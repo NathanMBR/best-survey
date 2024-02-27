7bfdf60ce82807a33e572bc6f71fba2076a39b92
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-02-27 19:35:00 -0300

    chore(backend): add HashString service

---
+++ backend/src/services/hash/HashString.ts
@@ -0,0 +1,13 @@
+import bcrypt from 'bcrypt'
+
+export class HashString {
+  constructor(private readonly saltRounds: number) {}
+
+  async execute(text: string): Promise<string> {
+    const salt = await bcrypt.genSalt(this.saltRounds)
+
+    const hash = await bcrypt.hash(text, salt)
+
+    return hash
+  }
+}
