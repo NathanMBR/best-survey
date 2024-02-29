f14ee51b4c76314b69c9ca56d52656abeec85206
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-02-29 16:42:53 -0300

    chore(backend): add FASTIFY_LOGGER environment variable

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -6,4 +6,9 @@ export const PORT = zod.coerce.number({
   invalid_type_error: 'PORT environment variable must be a number',
   description: 'PORT environment variable'
 }).positive().parse(process.env.PORT)
+
+export const FASTIFY_LOGGER = zod.string({
+  invalid_type_error: 'FASTIFY_LOGGER environment variable must be a string',
+  description: 'FASTIFY_LOGGER environment variable'
+}).optional().nullable().transform(value => value === 'true').parse(process.env.FASTIFY_LOGGER)
 /* eslint-enable camelcase */
