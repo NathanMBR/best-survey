346aeccf373592a1129948b18440164cf2f6a333
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-02-29 10:43:05 -0300

    refactor(backend): add ICompareHashedStringService

--- backend/src/services/hash/CompareHashedStringService.ts
+++ backend/src/services/hash/CompareHashedStringService.ts
@@ -1,7 +1,20 @@
 import bcrypt from 'bcrypt'
 
-export class CompareHashedStringService {
-  async execute(text: string, hash: string): Promise<boolean> {
+export namespace ICompareHashedStringService {
+  export type Request = {
+    text: string
+    hash: string
+  }
+
+  export type Response = Promise<boolean>
+}
+
+export interface ICompareHashedStringService {
+  execute: (request: ICompareHashedStringService.Request) => ICompareHashedStringService.Response
+}
+
+export class CompareHashedStringService implements ICompareHashedStringService {
+  async execute({ text, hash }: ICompareHashedStringService.Request) {
     const result = await bcrypt.compare(text, hash)
 
     return result
