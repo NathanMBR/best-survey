d51cfed9aaccfd77c8af16edadee71190903f560
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-02-29 16:43:07 -0300

    chore(backend): add userRoutes

---
+++ backend/src/main/routes/userRoutes.ts
@@ -0,0 +1,23 @@
+import {
+  FastifyInstance,
+  FastifyRegisterOptions,
+  FastifyPluginOptions,
+  HookHandlerDoneFunction
+} from 'fastify'
+
+import { makeCreateUserController } from '../factories'
+import { fastifyRouteAdapter } from './fastifyRouteAdapter'
+
+export const userRoutes = async (
+  app: FastifyInstance,
+  _options: FastifyRegisterOptions<FastifyPluginOptions>,
+  done: HookHandlerDoneFunction
+) => {
+  const createUserController = makeCreateUserController()
+
+  const createUserRoute = fastifyRouteAdapter(createUserController)
+
+  app.post('/', createUserRoute)
+
+  return done()
+}
