540d77a9d9153dcd9b4cbf4fb7f5d8f53e629fda
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-02-29 18:43:06 -0300

    chore(backend): add createApp

---
+++ backend/src/main/app/createApp.ts
@@ -0,0 +1,21 @@
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+
+import {
+  prisma,
+  FASTIFY_LOGGER
+} from '@/config'
+import { fastifyRoutes } from '../routes'
+
+export const createApp = async () => {
+  await prisma.$connect()
+
+  const app = fastify({
+    logger: FASTIFY_LOGGER
+  })
+
+  await app.register(cors)
+  await app.register(fastifyRoutes, { prefix: '/api' })
+
+  return app
+}
